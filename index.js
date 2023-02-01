518b37ab3951bfe87f144859ac9d69f3ec956c67
José A LX <lx08559@innovaccion.mx>
2023-01-31 23:08:06 -0400

    Primer commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function moverPosicionRandom(elm) {
+    elm.style.position = "absolute";
+    elm.style.top = Math.random() * (window.innerHeight - elm.offsetHeight) + 'px';
+    elm.style.left = Math.random() * (window.innerWidth - elm.offsetWidth) + 'px';
+}
+
+const btnSi = document.getElementById("btn_si");
+const btnNo = document.getElementById("btn_no");
+const modoSexo = document.getElementsByClassName("modo_sexo")[0];
+
+btnNo.addEventListener('mouseenter', function(e) { moverPosicionRandom(e.target) })
+
+btnSi.addEventListener('click', function(e) {
+    alert('Sabía que dirías que sí. Casémonos y tengamos hijos. TE AMO!!! ♥');
+    modoSexo.style.display = 'block';
+    btnNo.style.display = 'none';
+    const cancion = new Audio('link audio');
+    cancion.play();
+})
